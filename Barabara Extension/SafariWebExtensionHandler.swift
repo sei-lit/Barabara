ed76f7f5900891b55f1fbef7b7059eca85bfbe56
SeiOmori <sei.omori.22@gmail.com>
2023-05-12 14:02:10 +0900

    Initial Commit

---
+++ Barabara Extension/SafariWebExtensionHandler.swift
@@ -0,0 +1,24 @@
+//
+//  SafariWebExtensionHandler.swift
+//  Barabara Extension
+//
+//  Created by 大森青 on 2023/05/12.
+//
+
+import SafariServices
+import os.log
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+
+}
---
+++ Barabara/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Barabara
+//
+//  Created by 大森青 on 2023/05/12.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
